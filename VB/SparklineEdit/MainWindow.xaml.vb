1e9bb84db0f20ba38be68f318a3065505d2bb90c
DevExpressExampleBot <fakeEmail>
2023-12-06 16:21:59 +0400

    Source auto update [skip ci]

--- VB/SparklineEdit/MainWindow.xaml.vb
+++ VB/SparklineEdit/MainWindow.xaml.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Collections
 Imports System.Collections.ObjectModel
@@ -6,68 +5,32 @@ Imports System.Windows
 
 Namespace SparklineEdit
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private privateSourceCollection As IList
-		Public Property SourceCollection() As IList
-			Get
-				Return privateSourceCollection
-			End Get
-			Set(ByVal value As IList)
-				privateSourceCollection = value
-			End Set
-		End Property
+        Public Property SourceCollection As IList
 
-		Public Sub New()
-			InitializeComponent()
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.grid.DataContext = Me
+            GenerateData(7)
+        End Sub
 
-			grid.DataContext = Me
+        Private Sub GenerateData(ByVal n As Integer)
+            SourceCollection = New ObservableCollection(Of CustomElement)()
+            Dim rnd As Random = New Random()
+            For i As Integer = 1 To n
+                SourceCollection.Add(New CustomElement() With {.ArgumentColumn = New DateTime(2000, 1, i), .ValueColumn = rnd.Next(20), .FilteringColumn = 1})
+            Next
+        End Sub
+    End Class
 
-			GenerateData(7)
-		End Sub
+    Public Class CustomElement
 
-		Private Sub GenerateData(ByVal n As Integer)
-			SourceCollection = New ObservableCollection(Of CustomElement)()
+        Public Property ArgumentColumn As Object
 
-			Dim rnd As New Random()
-			For i As Integer = 1 To n
-				SourceCollection.Add(New CustomElement() With {.ArgumentColumn = New DateTime(2000, 1, i), .ValueColumn = rnd.Next(20), .FilteringColumn = 1})
-			Next i
+        Public Property ValueColumn As Integer
 
-
-		End Sub
-	End Class
-
-	Public Class CustomElement
-		Private privateArgumentColumn As Object
-		Public Property ArgumentColumn() As Object
-			Get
-				Return privateArgumentColumn
-			End Get
-			Set(ByVal value As Object)
-				privateArgumentColumn = value
-			End Set
-		End Property
-		Private privateValueColumn As Integer
-		Public Property ValueColumn() As Integer
-			Get
-				Return privateValueColumn
-			End Get
-			Set(ByVal value As Integer)
-				privateValueColumn = value
-			End Set
-		End Property
-		Private privateFilteringColumn As Integer
-		Public Property FilteringColumn() As Integer
-			Get
-				Return privateFilteringColumn
-			End Get
-			Set(ByVal value As Integer)
-				privateFilteringColumn = value
-			End Set
-		End Property
-	End Class
-
-
+        Public Property FilteringColumn As Integer
+    End Class
 End Namespace
